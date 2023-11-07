12e4c7b3e1b5d76186d399d7848063e6b065f4a0
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-07 15:30:35 +0100

    adding fonts

--- app/Header.js
+++ app/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Nav from './Nav';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header>
+      <Image
+        src="/large-star.png"
+        alt="Blog Logo"
+        width="50"
+        height="50"
+        quality="100"
+        priority
+      />
       <Nav />
     </header>
   );
--- app/layout.js
+++ app/layout.js
@@ -2,19 +2,23 @@ import { Inter } from 'next/font/google';
 import Header from './Header';
 import Footer from './Footer';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import { Roboto } from 'next/font/google';
+// const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  fallback: ['Helvetica Neue', 'sans-serif'],
+});
 
 export const metadata = {
   title: 'Bla Bla Blog',
-  description:
-    'A Blog about a self-taught developer, his struggles and his dick',
+  description: 'A Blog about a self-taught developer',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Header />
         {children}
         <Footer />
